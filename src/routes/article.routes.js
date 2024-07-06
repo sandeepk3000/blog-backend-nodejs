afe5e56d3c181c91c3b2273763b81011f8ee1ff5
sandeepk3000 <codejourney@gmail.com>
2024-07-05 22:17:26 -0700

    add route createArticle  as post , jwt and upload

---
+++ src/routes/article.routes.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { createArticle } from "../controllers/blog.controller.js"
+import verifyJWT from "../middlewares/auth.middleware.js"
+import { upload } from "../middlewares/multer.middleware.js"
+
+const router = express.Router()
+
+router.route("/createArticle").post(verifyJWT,upload.fields([
+    {
+        name:"thumbnail",
+        maxCount:1
+    }
+]),createArticle)
+
+export default router
