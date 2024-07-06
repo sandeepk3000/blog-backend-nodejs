268f50aaece0346fe09f92636f013ffa822e3026
sandeepk3000 <codejourney@gmail.com>
2024-07-05 21:56:37 -0700

    add user route api/v1/user

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 const app = express()
 import cors from "cors"
 import cookieParser from "cookie-parser"
-
+import userRoute from "./routes/user.routes.js"
 const corsOptions = {
     origin: "*",
     credentials: true
@@ -12,5 +12,6 @@ app.use(express.static("./public"))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.json({ limit: "16kb" }))
+app.use("/api/v1/user", userRoute)
 
 export { app }
