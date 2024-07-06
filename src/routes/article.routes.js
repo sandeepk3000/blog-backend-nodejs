1c6e48f55cb84be2bc130ae4ddd6458c76bd98be
sandeepk3000 <codejourney@gmail.com>
2024-07-05 22:17:53 -0700

    add route createArticle  as post , jwt and upload

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createArticle } from "../controllers/blog.controller.js"
+import { createArticle } from "../controllers/article.controller.js"
 import verifyJWT from "../middlewares/auth.middleware.js"
 import { upload } from "../middlewares/multer.middleware.js"
 
