f710319f38e63e6d42e24a13ed21e299574d5dd8
sandeepk3000 <codejourney@gmail.com>
2024-07-05 21:57:48 -0700

    add article route api/v1/article

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express()
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRoute from "./routes/user.routes.js"
+import articleRoute from "./routes/article.routes.js"
 const corsOptions = {
     origin: "*",
     credentials: true
@@ -12,6 +13,8 @@ app.use(express.static("./public"))
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.json({ limit: "16kb" }))
+
 app.use("/api/v1/user", userRoute)
 
+app.use("/api/v1/article", articleRoute)
 export { app }
