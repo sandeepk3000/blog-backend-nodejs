21c3a732605ae327cf9a850d41d7547552f7ccd6
sandeepk3000 <codejourney@gmail.com>
2024-07-06 19:17:52 -0700

    add route deleteArticle  as get

--- src/routes/article.routes.js
+++ src/routes/article.routes.js
@@ -1,9 +1,11 @@
 import express from "express"
-import { createArticle, updateArticle } from "../controllers/article.controller.js"
+import { createArticle, deleteArticle, updateArticle } from "../controllers/article.controller.js"
 import verifyJWT from "../middlewares/auth.middleware.js"
 import { upload } from "../middlewares/multer.middleware.js"
 
 const router = express.Router()
+
+
 
 router.route("/createArticle").post(verifyJWT,upload.fields([
     {
@@ -18,4 +20,6 @@ router.route("/updateArticle/:articleId").post(verifyJWT,upload.fields([
         maxCount:1
     }
 ]),updateArticle)
+
+router.route("/deleteArticle/:articleId").get(verifyJWT,deleteArticle)
 export default router
