5b78faf7d51212bf7408b56655bc848d224fd7db
sandeepk3000 <codejourney@gmail.com>
2024-07-07 21:17:42 -0700

    correct import statement of apiError.js to ApiError.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { User } from "../models/user.model.js";
 import ApiResponse from "../utils/ApiResponse.js";
-import { ApiError } from "../utils/apiError.js";
+import { ApiError } from "../utils/ApiError.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
 const sigunp = asyncHandler(async (req, res) => {
