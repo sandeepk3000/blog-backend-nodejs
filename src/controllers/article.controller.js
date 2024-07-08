77aebd955f4922ffc414c1c1c7f9091f404da9a9
sandeepk3000 <codejourney@gmail.com>
2024-07-07 20:29:24 -0700

    correct import statement of apiError.js to ApiError.js

--- src/controllers/article.controller.js
+++ src/controllers/article.controller.js
@@ -1,6 +1,6 @@
 import { Article } from "../models/article.model.js"
 import ApiResponse from "../utils/ApiResponse.js"
-import { ApiError } from "../utils/apiError.js"
+import { ApiError } from "../utils/ApiError.js"
 import asyncHandler from "../utils/asyncHandler.js"
 import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudinary.js"
 
