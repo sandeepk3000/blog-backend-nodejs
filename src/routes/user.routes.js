808a15bb4231f5a58b88281aa69c38f47d545f9d
sandeepk3000 <codejourney@gmail.com>
2024-07-07 22:02:42 -0700

    change name of sigunp to signup

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { sigunp,login,logout, getUser } from "../controllers/user.controller.js";
+import { signup,login,logout, getUser } from "../controllers/user.controller.js";
 import verifyJWT from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
-router.route("/signup").post(sigunp)
+router.route("/signup").post(signup)
 
 router.route("/login").post(login)
 
