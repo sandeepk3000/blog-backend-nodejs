e2f004a6e4bc89d2a34c2fcfad2da0306899e510
sandeepk3000 <codejourney@gmail.com>
2024-07-07 21:12:17 -0700

    merge origin/main

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,10 @@
 import mongoose from "mongoose"
-<<<<<<< HEAD
 const connectToDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(process.env.MONGODB_URL)
         // console.log("connectionInstance", connectionInstance)
     } catch (error) {
         console.log("error during connection ", error)
-=======
-
-const connectToDB = async()=>{
-    try {
-        const url = "mongodb+srv://geelucknow:ETlsFfb3xUPLBuNw@blogscluster.qmxsyx1.mongodb.net/skblogs?retryWrites=true&w=majority&appName=Blogscluster"
-        const connectionInstance = await mongoose.connect(url)
-        console.log("connectionInstance",connectionInstance)
-    } catch (error) {
-        console.log("error during connection ",error)
->>>>>>> origin/main
         process.exit(1)
     }
 
